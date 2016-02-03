8f6d59b9d4d6085dce301a4f36c381e89bc032f1
ericweidman <ericweidman@gmail.com>
2016-02-03 11:58:03 -0500

    Created player class

--- src/Game.java
+++ src/Game.java
@@ -4,43 +4,15 @@ import java.util.Scanner;
  * Created by ericweidman on 2/3/16.
  */
 public class Game {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 
         System.out.println("Welcome, traveller.");
-        System.out.println("What is your name?");
 
-        // alt + enter import
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Welcome, " + name + ".");
-
-        System.out.println("Choose your weapon [sword/mace]");
-        String weapon = scanner.nextLine();
-
-        if(weapon.equalsIgnoreCase("sword")){
-            System.out.println("A sword if a fine choice!");
-
-        }
-        else if (weapon.equalsIgnoreCase("mace")){
-            System.out.println("A mace is a fine choice!");
-        }
-        //Exceptions must be documented
-        //alt + enter
-        else {
-            throw new Exception("Invalid weapon!");
-        }
-
-        System.out.println("Choose your location [forest/tunnel]");
-        String location = scanner.nextLine();
-
-        if (location.equalsIgnoreCase("forest")) {
-            System.out.println("Entering forest...");
-        }
-        else if (location.equalsIgnoreCase("tunnel")){
-            System.out.println("Entering tunnel");
-        }
-        else{
-            throw new Exception("Invalid location!");
-        }
+        Player player = new Player();
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseLocation();
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,50 @@
+/**
+ * Created by ericweidman on 2/3/16.
+ */
+public class Player {
+    String name;
+    String weapon;
+    String location;
+
+
+    public void chooseName(){
+        System.out.println("What is your name?");
+        // alt + enter import
+        name = Game.scanner.nextLine();
+        System.out.println("Welcome, " + name + ".");
+    }
+
+    public void chooseWeapon() throws Exception {
+        System.out.println("Choose your weapon [sword/mace]");
+        weapon = Game.scanner.nextLine();
+
+        if(weapon.equalsIgnoreCase("sword")){
+            System.out.println("A sword if a fine choice!");
+
+        }
+        else if (weapon.equalsIgnoreCase("mace")){
+            System.out.println("A mace is a fine choice!");
+        }
+
+        else {
+            //Exceptions must be documented
+            //alt + enter
+            throw new Exception("Invalid weapon!");
+        }
+    }
+    public void chooseLocation() throws Exception {
+        System.out.println("Choose your location [forest/tunnel]");
+        location = Game.scanner.nextLine();
+
+        if (location.equalsIgnoreCase("forest")) {
+            System.out.println("Entering forest...");
+        }
+        else if (location.equalsIgnoreCase("tunnel")){
+            System.out.println("Entering tunnel");
+        }
+        else{
+            throw new Exception("Invalid location!");
+        }
+
+    }
+}
